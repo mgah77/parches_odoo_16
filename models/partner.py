1b773a5bfe417769ae1d70a49187cc8deda2cbdd
mgah77 <138524599+mgah77@users.noreply.github.com>
2026-01-14 22:50:43 -0300

    Update partner.py

--- models/partner.py
+++ models/partner.py
@@ -7,3 +7,10 @@ class res_partner_customer_provider(models.Model):
     is_provider = fields.Boolean(string ='Proveedor',default=False)
     time_limit = fields.Integer(string='Tiempo para pago en dias')
 
+    def check_vat_cl(self, vat):
+        """
+        Sobreescribe la validación del RUT chileno para que siempre sea válida.
+        De esta forma, se omite la validación del formato y dígito verificador.
+        """
+        # Simplemente devuelve True para que la validación siempre pase.
+        return True
