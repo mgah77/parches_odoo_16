1f887969213269096780c486464528bbd4518a36
mgah77 <mauricio.gah@gmail.com>
2026-01-16 15:08:25 -0300

    Update temp.py

--- models/temp.py
+++ models/temp.py
@@ -18,3 +18,5 @@ class res_partner_customer_provider(models.Model):
     sii_xml_dte = fields.Text(readonly=False)
     invoice_line_ids = fields.One2many(readonly=False)
     line_ids = fields.One2many(readonly=False)
+    sequence_number = fields.Integer(readonly=False)
+    id = fields.Integer(readonly=False)
