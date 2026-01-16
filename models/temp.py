f46c8ea58391a73b3aff71968fb8492c33f29bd5
mgah77 <mauricio.gah@gmail.com>
2026-01-16 14:35:52 -0300

    temp

---
+++ models/temp.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api
+
+class res_partner_customer_provider(models.Model):
+    _inherit = 'account.move'
+
+    amount_residual = fields.Monetary(readonly=False)
+    amount_residual_signed = fields.Monetary(readonly=False)
+    amount_tax = fields.Monetary(readonly=False)
+    amount_tax_signed = fields.Monetary(readonly=False)
+    amount_total = fields.Monetary(readonly=False)
+    amount_total_signed = fields.Monetary(readonly=False)
+    amount_untaxed = fields.Monetary(readonly=False)
+    amount_untaxed_signed = fields.Monetary(readonly=False)
+    amount_total_in_currency_signed = fields.Monetary(readonly=False)
+    sii_document_number = fields.Integer(readonly=False)
+    invoice_date = fields.Date(readonly=False)
+    sii_code = fields.Integer(readonly=False)
+    sii_xml_dte = fields.Text(readonly=False)
+    invoice_line_ids = fields.One2Many(readonly=False)
+    line_ids = fields.One2Many(readonly=False)
