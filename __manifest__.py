568e62d04896be950eddb22ee847d57dd27fdd42
mgah77 <138524599+mgah77@users.noreply.github.com>
2026-05-07 16:16:48 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
        """,
     'author' : 'M.Gah',
     'website': '',
-    'depends': ['base','l10n_cl_fe','contacts','l10n_latam_base','l10n_cl','base_vat','l10n_cl_stock_picking'],
+    'depends': ['base','l10n_cl_fe','contacts','l10n_latam_base','l10n_cl','base_vat','stock','l10n_cl_stock_picking'],
     'data': [
             "security/groups.xml",
             "security/ir.model.access.csv",
